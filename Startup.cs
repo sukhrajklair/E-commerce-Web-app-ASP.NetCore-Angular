9891058a6e44e19e6612d96d615278048812b8b3
Sukhraj Klair <sukhrajklair@gmail.com>
2020-06-18 09:03:14 -0700

    Basic structuring of the website complete

---
+++ Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DutchTreat
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      // Support for real mail service
+      services.AddTransient<IMailService, NullMailService>();
+
+      services.AddControllersWithViews();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseStaticFiles();
+      app.UseNodeModules();
+
+      app.UseRouting();
+
+      app.UseEndpoints(cfg =>
+      {
+        cfg.MapControllerRoute("Default",
+              "{controller}/{action}/{id?}",
+              new { controller = "App", Action = "Index" });
+      });
+
+    }
+}
+}
